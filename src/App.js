5ddbbbeba2a5ee56ac4a611107d3f0c6ee7ece97
anyanswer1125 <kll5893@gmail.com>
2024-08-21 02:36:33 +0900

    사이트 틀 잡음

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+
+  let post_title = '글 제목';
+
+  let [logo, setLogo] = useState('ReactPortfolio');
+  let [Title, setTitle] = useState(['테스트 제목 1','테스트 제목 2','테스트 제목 3']);
+
+
   return (
     <div className="App">
-      <div>
-        <h1>블로그 제작</h1>
+      <div className="black-nav">
+        <h1> { logo } </h1>
       </div>
+
+      <div className="list">
+        <h4> {Title[0]} </h4>
+        <p>2월 17일 발행</p>
+      </div>
+
+      <div className="list">
+        <h4> {Title[1]} </h4>
+        <p>2월 17일 발행</p>
+      </div>
+
+      <div className="list">
+        <h4> {Title[2]} </h4>
+        <p>2월 17일 발행</p>
+      </div>
+
     </div>
   );
 }
