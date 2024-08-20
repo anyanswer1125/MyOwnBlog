bf82979ad2abfe996a57daf1f52bd2c0cbd3f7b0
anyanswer1125 <kll5893@gmail.com>
2024-08-21 04:26:48 +0900

    Figma로 디자인제작 후 적용

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 
   let post_title = '글 제목';
 
-  let [logo, setLogo] = useState('ReactPortfolio');
+  let [logo, setLogo] = useState('HWAI Portfolio');
   let [Title, setTitle] = useState(['테스트 제목 1','테스트 제목 2','테스트 제목 3']);
   let [좋아요, 좋아요_수_변동] = useState(0);
 
@@ -20,24 +20,32 @@ function App() {
 
   return (
     <div className="App">
-      <div className="black-nav">
+      <div className="nav">
         <h1> { logo } </h1>
+        <button>Click Me!</button>
       </div>
 
-      <div className="list">
-        <h4> {Title[0]} <span onClick={함수}>❤️</span> {좋아요} </h4>
-        <p>2월 17일 발행</p>
+      <div className='Banner'></div>
+
+      <div className='lists-container'>
+
+        <div className="list">
+          <h4> {Title[0]} <span onClick={함수} className='button'> ❤️ </span> {좋아요} </h4>
+          <p>2월 17일 발행</p>
+        </div>
+
+        <div className="list">
+          <h4> {Title[1]} </h4>
+          <p>2월 17일 발행</p>
+        </div>
+
+        <div className="list">
+          <h4> {Title[2]} </h4>
+          <p>2월 17일 발행</p>
+        </div>
+
       </div>
 
-      <div className="list">
-        <h4> {Title[1]} </h4>
-        <p>2월 17일 발행</p>
-      </div>
-
-      <div className="list">
-        <h4> {Title[2]} </h4>
-        <p>2월 17일 발행</p>
-      </div>
 
     </div>
   );
