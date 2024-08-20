e08a633d5500976e8e100d469f1245e35a992e3c
anyanswer1125 <kll5893@gmail.com>
2024-08-21 08:00:07 +0900

    footer창 작업, 리스트 간 간격 수정

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import LoremIpsumImage6 from './Loremipsum6.png';
 
 function App() {
   let [logo] = useState('HWAI Portfolio');
-  let [Title] = useState(['테스트 제목 1','테스트 제목 2','테스트 제목 3','테스트 제목 4','테스트 제목 5','테스트 제목 6']);
+  let [Title] = useState(['테스트 제목 1', '테스트 제목 2', '테스트 제목 3', '테스트 제목 4', '테스트 제목 5', '테스트 제목 6']);
   let [좋아요, 좋아요_수_변동] = useState([41, 39, 37, 46, 182, 378]);
 
   function 함수(index) {
@@ -27,61 +27,65 @@ function App() {
 
       <div className='Banner'></div>
 
-      <div className='lists-container'>
-        <div className="list">
-          <h4>{Title[0]}</h4>
-          <img src={LoremIpsumImage} alt='Lorem1'></img>
-          <p>8월 21일 발행 
-            <span onClick={() => 함수(0)} className='social_function'> ❤️ {좋아요[0]}</span>
-          </p>
+      <div className='content'>
+        <div className='lists-container'>
+          <div className="list">
+            <h4>{Title[0]}</h4>
+            <img src={LoremIpsumImage} alt='Lorem1'></img>
+            <p>8월 21일 발행
+              <span onClick={() => 함수(0)} className='social_function'> ❤️ {좋아요[0]}</span>
+            </p>
+          </div>
+
+          <div className="list">
+            <h4>{Title[1]}</h4>
+            <img src={LoremIpsumImage2} alt='Lorem2'></img>
+            <p>8월 4일 발행
+              <span onClick={() => 함수(1)} className='social_function'> ❤️ {좋아요[1]}</span>
+            </p>
+          </div>
+
+          <div className="list">
+            <h4>{Title[2]}</h4>
+            <img src={LoremIpsumImage3} alt='Lorem3'></img>
+            <p>7월 26일 발행
+              <span onClick={() => 함수(2)} className='social_function'> ❤️ {좋아요[2]}</span>
+            </p>
+          </div>
         </div>
 
-        <div className="list">
-          <h4>{Title[1]}</h4>
-          <img src={LoremIpsumImage2} alt='Lorem2'></img>
-          <p>8월 4일 발행 
-            <span onClick={() => 함수(1)} className='social_function'> ❤️ {좋아요[1]}</span>
-          </p>
+        <div className='lists-container'>
+          <div className="list">
+            <h4>{Title[3]}</h4>
+            <img src={LoremIpsumImage4} alt='Lorem4'></img>
+            <p>7월 13일 발행
+              <span onClick={() => 함수(3)} className='social_function'> ❤️ {좋아요[3]}</span>
+            </p>
+          </div>
+
+          <div className="list">
+            <h4>{Title[4]}</h4>
+            <img src={LoremIpsumImage5} alt='Lorem5'></img>
+            <p>7월 2일 발행
+              <span onClick={() => 함수(4)} className='social_function'> ❤️ {좋아요[4]}</span>
+            </p>
+          </div>
+
+          <div className="list">
+            <h4>{Title[5]}</h4>
+            <img src={LoremIpsumImage6} alt='Lorem6'></img>
+            <p>6월 12일 발행
+              <span onClick={() => 함수(5)} className='social_function'> ❤️ {좋아요[5]}</span>
+            </p>
+          </div>
         </div>
 
-        <div className="list">
-          <h4>{Title[2]}</h4>
-          <img src={LoremIpsumImage3} alt='Lorem3'></img>
-          <p>7월 26일 발행 
-            <span onClick={() => 함수(2)} className='social_function'> ❤️ {좋아요[2]}</span>
-          </p>
+        <div className='more'>
+          <h1>새로운 피드가 없네요..</h1>
+          <button>인기 게시글 둘러보기</button>
         </div>
       </div>
 
-      <div className='lists-container'>
-        <div className="list">
-          <h4>{Title[3]}</h4>
-          <img src={LoremIpsumImage4} alt='Lorem4'></img>
-          <p>7월 13일 발행 
-            <span onClick={() => 함수(3)} className='social_function'> ❤️ {좋아요[3]}</span>
-          </p>
-        </div>
-
-        <div className="list">
-          <h4>{Title[4]}</h4>
-          <img src={LoremIpsumImage5} alt='Lorem5'></img>
-          <p>7월 2일 발행 
-            <span onClick={() => 함수(4)} className='social_function'> ❤️ {좋아요[4]}</span>
-          </p>
-        </div>
-
-        <div className="list">
-          <h4>{Title[5]}</h4>
-          <img src={LoremIpsumImage6} alt='Lorem6'></img>
-          <p>6월 12일 발행 
-            <span onClick={() => 함수(5)} className='social_function'> ❤️ {좋아요[5]}</span>
-          </p>
-        </div>
-      </div>
-      <div className='more'>
-        <h1>새로운 피드가 없네요</h1>
-        <button>인기 게시글 둘러보기</button>
-      </div>
       <footer className="footer">
         <p>&copy; 2024 HWAI Portfolio. All rights reserved.</p>
         <p>Contact: Lorem-ipsum@dolor.com</p>
